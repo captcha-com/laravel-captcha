527745c85005c9eb41f10a5be0298ba6a537f147
BotDetect Captcha <botdetect.support@captcha.com>
2015-11-01 00:42:19 +0700

    improved load library and override captcha config

--- src/Helpers/LibraryLoaderHelper.php
+++ src/Helpers/LibraryLoaderHelper.php
@@ -18,22 +18,50 @@ final class LibraryLoaderHelper {
      */
     public static function Load($p_Config = array()) {
         // load bd php library
-        self::IncludeFile(Path::GetBotDetectFilePathInLibrary());
-
-        // user's captcha config file
-        $userConfig = new UserCaptchaConfiguration();
-        if (array_key_exists('CaptchaId', $p_Config) && array_key_exists('CaptchaConfigFilePath', $p_Config)) {
-            $captchaConfigPath = $p_Config['CaptchaConfigFilePath'];
-            $captchaId = $p_Config['CaptchaId'];
-            $userConfig->StorePath($captchaId, $captchaConfigPath);
-        }
+        self::LoadBotDetectLibrary();
 
         // load the captcha configuration defaults
-        self::IncludeFile(Path::GetCaptchaConfigDefaultsFilePath());
+        self::LoadCaptchaConfigDefaults();
 
-        // load user's captcha configuration
-        $userCaptchaConfigFilePath = $userConfig->GetPhysicalPath();
-        self::IncludeFile($userCaptchaConfigFilePath);
+        // load user's captcha config
+        self::LoadUserCaptchaConfig($p_Config);
+    }
+
+    /**
+     * Load BotDetect CAPTCHA Library.
+     *
+     * @return void
+     */
+    private static function LoadBotDetectLibrary() {
+        self::IncludeFile(Path::GetBotDetectFilePathInLibrary());
+    }
+
+    /**
+     * Load the captcha configuration defaults.
+     *
+     * @return void
+     */
+    private static function LoadCaptchaConfigDefaults() {
+        self::IncludeFile(Path::GetCaptchaConfigDefaultsFilePath());
+    }
+    
+    /**
+     * Load user's captcha configuration.
+     *
+     * @param array  $p_Config
+     */
+    private static function LoadUserCaptchaConfig($p_Config) {
+        $userConfig = new UserCaptchaConfiguration();
+
+        // store user's captcha config file path
+        if (array_key_exists('CaptchaId', $p_Config) &&
+            array_key_exists('CaptchaConfigFilePath', $p_Config)
+        ) {
+            $userConfig->StorePath($p_Config['CaptchaId'], $p_Config['CaptchaConfigFilePath']);
+        }
+
+        $configFilePath = $userConfig->GetPhysicalPath();
+        self::IncludeFile($configFilePath);
     }
 
     /**
