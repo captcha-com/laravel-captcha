30452a17cb524015491b619fd06b001c35f89a90
BotDetect Captcha <botdetect.support@captcha.com>
2015-12-08 18:15:15 +0700

    update version to 3.3.0

--- src/Integration/BotDetectCaptcha.php
+++ src/Integration/BotDetectCaptcha.php
@@ -48,5 +48,5 @@ class BotDetectCaptcha {
 // static field initialization
 BotDetectCaptcha::$ProductInfo = [
     'name' => 'BotDetect PHP Captcha integration for the Laravel framework', 
-    'version' => '3.2.1'
+    'version' => '3.3.0'
 ];
