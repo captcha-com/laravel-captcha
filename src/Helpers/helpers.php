b862b1b9dcc3b1d6710532e80f36a0517eddc53f
botdetect.support <botdetect.support@captcha.com>
2016-01-08 18:02:15 +0700

    added captcha_is_solved and captcha_reset helper functions

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -83,7 +83,9 @@ if (! function_exists('captcha_image_html')) {
     function captcha_image_html($captchaId = '')
     {
         if (empty($captchaId)) {
-            throw new InvalidArgumentException('The "captcha_image_html" helper function requires you to pass the configuration option defined in config/captcha.php file.');
+            $errorMessages  = 'The "captcha_image_html" helper function requires you to pass the configuration option defined in config/captcha.php file. ';
+            $errorMessages .= 'For example: captcha_image_html(\'LoginCaptcha\')';
+            throw new InvalidArgumentException($errorMessages);
         }
 
         $captcha = captcha_instance($captchaId);
@@ -104,6 +106,51 @@ if (! function_exists('captcha_validate')) {
         $captchaId = find_captcha_id_in_form_data(\Request::all());
         $captcha = captcha_instance($captchaId);
         return $captcha->Validate($userInput, $instanceId);
+    }
+}
+
+if (! function_exists('captcha_is_solved')) {
+    /**
+     * Check Captcha is solved or not.
+     *
+     * @param string  $captchaId
+     * @return bool
+     */
+    function captcha_is_solved($captchaId = '')
+    {
+        if (empty($captchaId)) {
+            $errorMessages  = 'The "captcha_is_solved" helper function requires you to pass the configuration option defined in config/captcha.php file. '; 
+            $errorMessages .= 'For example: captcha_is_solved(\'LoginCaptcha\');';
+            throw new InvalidArgumentException($errorMessages);
+        }
+
+        $captcha = captcha_instance($captchaId);
+        return $captcha->IsSolved;
+    }
+}
+
+if (! function_exists('captcha_reset')) {
+    /**
+     * Reset captcha for current instance.
+     *
+     * @param string  $captchaId
+     * @return bool
+     */
+    function captcha_reset($captchaId = '')
+    {
+        if (empty($captchaId) && \Request::isMethod('get')) {
+            $errorMessages  = 'The "captcha_reset" helper function requires you to pass the configuration option defined in config/captcha.php file on HTTP GET request. '; 
+            $errorMessages .= 'For example: captcha_reset(\'LoginCaptcha\');';
+            throw new InvalidArgumentException($errorMessages);
+        }
+
+        if (empty($captchaId) && \Request::isMethod('post')) {
+            $captchaId = find_captcha_id_in_form_data(\Request::all());
+        }
+
+        $captcha = captcha_instance($captchaId);
+
+        return $captcha->Reset();
     }
 }
 
