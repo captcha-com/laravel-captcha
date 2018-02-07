051f1e71a3f37d0dc665a9242dffb678231d2c81
botdetect.support <botdetect.support@captcha.com>
2018-02-08 00:56:49 +0700

    update code to compatible with new version of bd php

--- src/BotDetectCaptcha.php
+++ src/BotDetectCaptcha.php
@@ -30,7 +30,7 @@ class BotDetectCaptcha
      * @param  string  $configName
      * @return void
      */
-    public function __construct($configName)
+    public function __construct($configName, $captchaInstanceId = null)
     {
         self::$instance = $this;
 
@@ -53,7 +53,7 @@ class BotDetectCaptcha
         UserCaptchaConfiguration::save($config);
 
         // create a BotDetect Captcha object instance
-        $this->initCaptcha($config);
+        $this->initCaptcha($config, $captchaInstanceId);
     }
 
     /**
@@ -62,11 +62,11 @@ class BotDetectCaptcha
      * @param  array  $config
      * @return void
      */
-    public function initCaptcha(array $config)
+    public function initCaptcha(array $config, $captchaInstanceId = null)
     {
         // set captchaId and create an instance of Captcha
         $captchaId = array_key_exists('CaptchaId', $config) ? $config['CaptchaId'] : 'defaultCaptchaId';
-        $this->captcha = new \Captcha($captchaId);
+        $this->captcha = new \Captcha($captchaId, $captchaInstanceId);
 
         // set user's input id
         if (array_key_exists('UserInputID', $config)) {
@@ -84,6 +84,13 @@ class BotDetectCaptcha
         return self::$instance;
     }
 
+    /**
+     * @return object
+     */
+    public function getCaptchaInstance() {
+        return $this->captcha;
+    }
+
     public function __call($method, $args = array())
     {
         if (method_exists($this, $method)) {
@@ -93,6 +100,10 @@ class BotDetectCaptcha
         if (method_exists($this->captcha, $method)) {
             return call_user_func_array(array($this->captcha, $method), $args);
         }
+
+        if (method_exists($this->captcha->get_CaptchaBase(), $method)) {
+            return call_user_func_array(array($this->captcha->get_CaptchaBase(), $method), $args);
+        }
     }
 
     /**
@@ -100,6 +111,10 @@ class BotDetectCaptcha
      */
     public function __get($name)
     {
+        if (method_exists($this->captcha->get_CaptchaBase(), ($method = 'get_'.$name))) {
+            return $this->captcha->get_CaptchaBase()->$method();
+        }
+
         if (method_exists($this->captcha, ($method = 'get_'.$name))) {
             return $this->captcha->$method();
         }
@@ -111,6 +126,10 @@ class BotDetectCaptcha
 
     public function __isset($name)
     {
+        if (method_exists($this->captcha->get_CaptchaBase(), ($method = 'isset_'.$name))) {
+            return $this->captcha->get_CaptchaBase()->$method();
+        }
+
         if (method_exists($this->captcha, ($method = 'isset_'.$name))) {
             return $this->captcha->$method();
         } 
@@ -122,6 +141,10 @@ class BotDetectCaptcha
 
     public function __set($name, $value)
     {
+        if (method_exists($this->captcha->get_CaptchaBase(), ($method = 'set_'.$name))) {
+            return $this->captcha->get_CaptchaBase()->$method($value);
+        }
+
         if (method_exists($this->captcha, ($method = 'set_'.$name))) {
             $this->captcha->$method($value);
         } else if (method_exists($this, ($method = 'set_'.$name))) {
@@ -131,6 +154,10 @@ class BotDetectCaptcha
 
     public function __unset($name)
     {
+        if (method_exists($this->captcha->get_CaptchaBase(), ($method = 'unset_'.$name))) {
+            return $this->captcha->get_CaptchaBase()->$method();
+        }
+
         if (method_exists($this->captcha, ($method = 'unset_'.$name))) {
             $this->captcha->$method();
         } else if (method_exists($this, ($method = 'unset_'.$name))) {
--- src/Controllers/CaptchaHandlerController.php
+++ src/Controllers/CaptchaHandlerController.php
@@ -49,6 +49,7 @@ class CaptchaHandlerController extends Controller
             }
 
             $command = \BDC_CaptchaHttpCommand::FromQuerystring($commandString);
+            $responseBody = '';
             switch ($command) {
                 case \BDC_CaptchaHttpCommand::GetImage:
                     $responseBody = $this->getImage();
@@ -56,8 +57,14 @@ class CaptchaHandlerController extends Controller
                 case \BDC_CaptchaHttpCommand::GetSound:
                     $responseBody = $this->getSound();
                     break;
-                case \BDC_CaptchaHttpCommand::getValidationResult:
+                case \BDC_CaptchaHttpCommand::GetValidationResult:
                     $responseBody = $this->getValidationResult();
+                    break;
+                case \BDC_CaptchaHttpCommand::GetInitScriptInclude:
+                    $responseBody = $this->getInitScriptInclude();
+                    break;
+                case \BDC_CaptchaHttpCommand::GetP:
+                    $responseBody = $this->getP();
                     break;
                 default:
                     \BDC_HttpHelper::BadRequest('command');
@@ -78,11 +85,17 @@ class CaptchaHandlerController extends Controller
     private function getBotDetectCaptchaInstance()
     {
         $captchaId = $this->getUrlParameter('c');
-        if (!is_null($captchaId) && preg_match('/^(\w+)$/ui', $captchaId)) {
-            return new BotDetectCaptcha($captchaId);
-        } else {
+        if (is_null($captchaId) || !preg_match('/^(\w+)$/ui', $captchaId)) {
             $this->badRequest('Invalid captcha id.');
         }
+
+        $captchaInstanceId = $this->getUrlParameter('t');
+        if (is_null($captchaInstanceId) || !(32 == strlen($captchaInstanceId) &&
+                (1 === preg_match("/^([a-f0-9]+)$/u", $captchaInstanceId)))) {
+            $this->badRequest('Invalid instance id.');
+        }
+
+        return new BotDetectCaptcha($captchaId, $captchaInstanceId);
     }
 
     /**
@@ -129,6 +142,16 @@ class CaptchaHandlerController extends Controller
             \BDC_HttpHelper::BadRequest('instance');
         }
 
+        $libVersion = \BDC_CaptchaBase::$ProductInfo['version'];
+        if(version_compare($libVersion, '4.2.0') >= 0) {
+            if (!$this->captcha->CaptchaBase->IsInstanceIdExisted($instanceId)) {
+                \BDC_HttpHelper::BadRequest('Instance doesn\'t exist in session');
+            }
+        }
+
+        // image generation invalidates sound cache, if any
+        $this->clearSoundData($instanceId);
+
         // response headers
         \BDC_HttpHelper::DisallowCache();
 
@@ -152,7 +175,6 @@ class CaptchaHandlerController extends Controller
     /**
      * Generate a Captcha sound.
      *
-     * @return image
      */
     public function getSound()
     {
@@ -166,6 +188,22 @@ class CaptchaHandlerController extends Controller
             \BDC_HttpHelper::BadRequest('instance');
         }
 
+        $libVersion = \BDC_CaptchaBase::$ProductInfo['version'];
+        if(version_compare($libVersion, '4.2.0') >= 0) {
+            if (!$this->captcha->CaptchaBase->IsInstanceIdExisted($instanceId)) {
+                \BDC_HttpHelper::BadRequest('Instance doesn\'t exist in session');
+            }
+        }
+
+        $soundBytes = $this->getSoundData($this->captcha->getCaptchaInstance(), $instanceId);
+
+        if (is_null($soundBytes)) {
+            \BDC_HttpHelper::BadRequest('Please reload the form page before requesting another Captcha sound');
+            exit;
+        }
+
+        $totalSize = strlen($soundBytes);
+
         // response headers
         \BDC_HttpHelper::SmartDisallowCache();
 
@@ -174,9 +212,139 @@ class CaptchaHandlerController extends Controller
         header("Content-Type: {$mimeType}");
         header('Content-Transfer-Encoding: binary');
 
-        // sound generation
-        $rawSound = $this->captcha->CaptchaBase->GetSound($instanceId);
+        if (!array_key_exists('d', $_GET)) { // javascript player not used, we send the file directly as a download
+            $downloadId = \BDC_CryptoHelper::GenerateGuid();
+            header("Content-Disposition: attachment; filename=captcha_{$downloadId}.wav");
+        }
+
+        if ($this->detectIosRangeRequest()) { // iPhone/iPad sound issues workaround: chunked response for iOS clients
+            // sound byte subset
+            $range = $this->getSoundByteRange();
+            $rangeStart = $range['start'];
+            $rangeEnd = $range['end'];
+            $rangeSize = $rangeEnd - $rangeStart + 1;
+
+            // initial iOS 6.0.1 testing; leaving as fallback since we can't be sure it won't happen again:
+            // we depend on observed behavior of invalid range requests to detect
+            // end of sound playback, cleanup and tell AppleCoreMedia to stop requesting
+            // invalid "bytes=rangeEnd-rangeEnd" ranges in an infinite(?) loop
+            if ($rangeStart == $rangeEnd || $rangeEnd > $totalSize) {
+                \BDC_HttpHelper::BadRequest('invalid byte range');
+            }
+
+            $rangeBytes = substr($soundBytes, $rangeStart, $rangeSize);
+
+            // partial content response with the requested byte range
+            header('HTTP/1.1 206 Partial Content');
+            header('Accept-Ranges: bytes');
+            header("Content-Length: {$rangeSize}");
+            header("Content-Range: bytes {$rangeStart}-{$rangeEnd}/{$totalSize}");
+            return $rangeBytes; // chrome needs this kind of response to be able to replay Html5 audio
+        } else if ($this->detectFakeRangeRequest()) {
+            header('Accept-Ranges: bytes');
+            header("Content-Length: {$totalSize}");
+            $end = $totalSize - 1;
+            header("Content-Range: bytes 0-{$end}/{$totalSize}");
+            return $soundBytes;
+        } else { // regular sound request
+            header('Accept-Ranges: none');
+            header("Content-Length: {$totalSize}");
+            return $soundBytes;
+        }
+    }
+
+    public function GetSoundData($p_Captcha, $p_InstanceId) {
+        $shouldCache = (
+            ($p_Captcha->SoundRegenerationMode == \SoundRegenerationMode::None) || // no sound regeneration allowed, so we must cache the first and only generated sound
+            $this->detectIosRangeRequest() // keep the same Captcha sound across all chunked iOS requests
+        );
+
+        if ($shouldCache) {
+            $loaded = $this->loadSoundData($p_InstanceId);
+            if (!is_null($loaded)) {
+                return $loaded;
+            }
+        } else {
+            $this->clearSoundData($p_InstanceId);
+        }
+
+        $soundBytes = $this->generateSoundData($p_Captcha, $p_InstanceId);
+        if ($shouldCache) {
+            $this->saveSoundData($p_InstanceId, $soundBytes);
+        }
+        return $soundBytes;
+    }
+
+    private function generateSoundData($p_Captcha, $p_InstanceId) {
+        $rawSound = $p_Captcha->CaptchaBase->GetSound($p_InstanceId);
+        $p_Captcha->CaptchaBase->SaveCodeCollection(); // always record sound generation count
         return $rawSound;
+    }
+
+    private function saveSoundData($p_InstanceId, $p_SoundBytes) {
+        LA_Session_Save("BDC_Cached_SoundData_" . $p_InstanceId, $p_SoundBytes);
+    }
+
+    private function loadSoundData($p_InstanceId) {
+        return LA_Session_Load("BDC_Cached_SoundData_" . $p_InstanceId);
+    }
+
+    private function clearSoundData($p_InstanceId) {
+        LA_Session_Clear("BDC_Cached_SoundData_" . $p_InstanceId);
+    }
+
+
+    // Instead of relying on unreliable user agent checks, we detect the iOS sound
+    // requests by the Http headers they will always contain
+    private function detectIosRangeRequest() {
+        $detected = false;
+
+        if(array_key_exists('HTTP_RANGE', $_SERVER) &&
+            \BDC_StringHelper::HasValue($_SERVER['HTTP_RANGE'])) {
+
+            // Safari on MacOS and all browsers on <= iOS 10.x
+            if(array_key_exists('HTTP_X_PLAYBACK_SESSION_ID', $_SERVER) &&
+                \BDC_StringHelper::HasValue($_SERVER['HTTP_X_PLAYBACK_SESSION_ID'])) {
+                $detected = true;
+            }
+
+            // all browsers on iOS 11.x and later
+            if(array_key_exists('User-Agent', $_SERVER) &&
+                \BDC_StringHelper::HasValue($_SERVER['User-Agent'])) {
+                $userAgent = $_SERVER['User-Agent'];
+                if(strpos($userAgent, "iPhone OS") !== false || strpos($userAgent, "iPad") !== false) { // is iPhone or iPad
+                    $detected = true;
+                }
+            }
+        }
+        return $detected;
+    }
+
+    private function getSoundByteRange() {
+        // chunked requests must include the desired byte range
+        $rangeStr = $_SERVER['HTTP_RANGE'];
+        if (!\BDC_StringHelper::HasValue($rangeStr)) {
+            return;
+        }
+
+        $matches = array();
+        preg_match_all('/bytes=([0-9]+)-([0-9]+)/', $rangeStr, $matches);
+        return array(
+            'start' => (int) $matches[1][0],
+            'end'   => (int) $matches[2][0]
+        );
+    }
+
+    private function detectFakeRangeRequest() {
+        $detected = false;
+        if (array_key_exists('HTTP_RANGE', $_SERVER)) {
+            $rangeStr = $_SERVER['HTTP_RANGE'];
+            if (\BDC_StringHelper::HasValue($rangeStr) &&
+                preg_match('/bytes=0-$/', $rangeStr)) {
+                $detected = true;
+            }
+        }
+        return $detected;
     }
 
     /**
@@ -209,6 +377,38 @@ class CaptchaHandlerController extends Controller
         $resultJson = $this->getJsonValidationResult($result);
 
         return $resultJson;
+    }
+
+    public function getInitScriptInclude() {
+        // saved data for the specified Captcha object in the application
+        if (is_null($this->captcha)) {
+            \BDC_HttpHelper::BadRequest('captcha');
+        }
+
+        // identifier of the particular Captcha object instance
+        $instanceId = $this->getInstanceId();
+        if (is_null($instanceId)) {
+            \BDC_HttpHelper::BadRequest('instance');
+        }
+
+        // response MIME type & headers
+        header('Content-Type: text/javascript');
+        header('X-Robots-Tag: noindex, nofollow, noarchive, nosnippet');
+
+        $result = "(function() {\r\n";
+
+        // add init script
+        $result .= \BDC_CaptchaScriptsHelper::GetInitScriptMarkup($this->captcha->getCaptchaInstance(), $instanceId);
+
+        // add remote scripts if enabled
+        if ($this->captcha->RemoteScriptEnabled) {
+            $result .= "\r\n";
+            $result .= \BDC_CaptchaScriptsHelper::GetRemoteScript($this->captcha->getCaptchaInstance());
+        }
+
+        // close a self-invoking functions
+        $result .= "\r\n})();";
+        return $result;
     }
 
     /**
@@ -298,5 +498,34 @@ class CaptchaHandlerController extends Controller
         header('Content-Type: text/plain');
         echo $message;
         exit;
+    }
+
+    public function GetP() {
+        if (is_null($this->captcha)) {
+            \BDC_HttpHelper::BadRequest('captcha');
+        }
+
+        // identifier of the particular Captcha object instance
+        $instanceId = $this->getInstanceId();
+        if (is_null($instanceId)) {
+            \BDC_HttpHelper::BadRequest('instance');
+        }
+
+        // create new one
+        $p = new \P($instanceId);
+
+        // save
+        LA_Session_Clear($this->captcha->CaptchaBase->getPPersistenceKey($instanceId));
+        LA_Session_Save($this->captcha->CaptchaBase->getPPersistenceKey($instanceId), $p);
+
+        // response data
+        $response = "{\"sp\":\"{$p->GSP()}\",\"hs\":\"{$p->GHs()}\"}";
+
+        // response MIME type & headers
+        header('Content-Type: application/json');
+        header('X-Robots-Tag: noindex, nofollow, noarchive, nosnippet');
+        \BDC_HttpHelper::SmartDisallowCache();
+
+        return $response;
     }
 }
