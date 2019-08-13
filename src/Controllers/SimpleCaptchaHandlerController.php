e438e9c37ce7a6ce2c74422decc4e386e4a60e5e
botdetect.support <botdetect.support@captcha.com>
2019-08-13 10:56:05 +0000

    Fixed a bug causing error: Too few arguments to function BDC_SimpleCaptchaScriptsHelper::GetRemoteScript()

--- src/Controllers/SimpleCaptchaHandlerController.php
+++ src/Controllers/SimpleCaptchaHandlerController.php
@@ -547,10 +547,20 @@ class SimpleCaptchaHandlerController extends Controller
         // 3. load remote scripts if enabled
         if ($this->captcha->RemoteScriptEnabled) {
             $script .= "\r\n";
-            $script .= \BDC_SimpleCaptchaScriptsHelper::GetRemoteScript($this->captcha);
+            $script .= \BDC_SimpleCaptchaScriptsHelper::GetRemoteScript($this->captcha, $this->getClientSideFramework());
         }
 
         return $script;
+    }
+
+    private function getClientSideFramework()
+    {
+        $clientSide = $this->getUrlParameter('cs');
+        if (\BDC_StringHelper::HasValue($clientSide)) {
+            $clientSide = \BDC_StringHelper::Normalize($clientSide);
+            return $clientSide;
+        }
+        return null;
     }
 
     private function getWebResource($p_Resource, $p_MimeType, $hasEtag = true)
